d2b64fa6b01c6f0b69fee351b1e08c4e50760757
marek.laskowski <SDfsadf>
2017-06-16 12:39:07 -0400

    download and display the xml file

--- app/src/main/java/com/example/mareklaskowski/cbc_xml_summer_2017/MainActivity.java
+++ app/src/main/java/com/example/mareklaskowski/cbc_xml_summer_2017/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.mareklaskowski.cbc_xml_summer_2017;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //here we use our AsyncTask class to do the xml download
+
+        //instantiate a URL object used to point to the xml resource
+        URL url = null;
+        try{
+            url = new URL("http://www.cbc.ca/cmlink/rss-topstories");
+        }catch (Exception e){
+            Log.d("AN EXCEPTION HAPPENED", e.getMessage());
+        }
+
+        //use our custom AsyncTask to download (and process) the xml resource available at the indicated URL
+        //call a framework method that will eventually call doInBackground found below
+        new DownloadAndParseXMLTask().execute(url);
     }
-    
+
     private class DownloadAndParseXMLTask extends AsyncTask<URL, Integer, Long> {
 
         //FYI - doInBackground is a "slot" method where we have to fill in its behaviour to compile
@@ -39,7 +57,44 @@ public class MainActivity extends AppCompatActivity {
         //FYI the methods below are "hook" methods because they already have default implementations that we can override
     }
 
+    /*
+    download and parse the xml file indicated by the url
+     */
     public void downloadFile(URL url){
 
+        //we need a try-catch block right away - almost all this stuff can throw exceptions
+        try{
+            //create a new http url connection
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            //create an InputStreamReader that we can use to read data from the http url connection
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            //do something with the data
+            //display the downloaded xml for debugging purposes
+            //wrap the inputStreamReader in a BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            //create a string variable to store each line as we read it from bufferedReader
+            String inputline = null;
+            //here's a cool one liner to read a line and make sure it's not null (in case of EOF)
+            while((inputline = bufferedReader.readLine()) != null){
+                //display for debugging purposes
+                Log.d("HEADLINES DOWNLOADED", inputline);
+            }
+
+        } catch (Exception e){
+            Log.d("DOWNLOAD ERROR", e.getMessage());
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
