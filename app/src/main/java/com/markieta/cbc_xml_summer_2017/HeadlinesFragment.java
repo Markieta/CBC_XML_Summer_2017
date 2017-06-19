84529803e468670b4e7c6f451dd0521475c0eb11
Christopher Markieta <markietachristopher@gmail.com>
2017-06-19 13:31:03 -0400

    Removing unnecessary comments

--- app/src/main/java/com/markieta/cbc_xml_summer_2017/HeadlinesFragment.java
+++ app/src/main/java/com/markieta/cbc_xml_summer_2017/HeadlinesFragment.java
@@ -16,7 +16,6 @@
 package com.markieta.cbc_xml_summer_2017;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
@@ -26,9 +25,7 @@ import android.widget.ListView;
 public class HeadlinesFragment extends ListFragment {
     OnHeadlineSelectedListener mCallback;
 
-    // The container Activity must implement this interface so the frag can deliver messages
     interface OnHeadlineSelectedListener {
-        /** Called by HeadlinesFragment when a list item is selected */
         void onArticleSelected(int position);
     }
 
@@ -37,8 +34,6 @@ public class HeadlinesFragment extends ListFragment {
         super.onCreate(savedInstanceState);
 
         int layout = android.R.layout.simple_list_item_activated_1;
-
-        // Create an array adapter for the list view, using the Ipsum headlines array
         setListAdapter(new ArrayAdapter<>(getActivity(), layout, Ipsum.Headlines));
     }
 
@@ -46,8 +41,6 @@ public class HeadlinesFragment extends ListFragment {
     public void onStart() {
         super.onStart();
 
-        // When in two-pane layout, set the listview to highlight the selected list item
-        // (We do this during onStart because at the point the listview is available.)
         if (getFragmentManager().findFragmentById(R.id.article_fragment) != null) {
             getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         }
@@ -57,8 +50,6 @@ public class HeadlinesFragment extends ListFragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
-        // This makes sure that the container activity has implemented
-        // the callback interface. If not, it throws an exception.
         try {
             mCallback = (OnHeadlineSelectedListener) activity;
         } catch (ClassCastException e) {
@@ -69,10 +60,7 @@ public class HeadlinesFragment extends ListFragment {
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        // Notify the parent activity of selected item
         mCallback.onArticleSelected(position);
-
-        // Set the item as checked to be highlighted when in two-pane layout
         getListView().setItemChecked(position, true);
     }
 }
